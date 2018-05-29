1fba74f283a4422fbcf28e560e8380db36e17a63
Gregor Schumann <gregor.schumann@sap.com>
2018-05-29 16:00:38 +0200

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,7 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+import '../imports/ui/templates/header.html';
+import '../imports/ui/templates/footer.html';
